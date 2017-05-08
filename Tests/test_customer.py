14d2e21271821fb194707daea87600214a2e622f
short880 <shortjustin880@gmail.com>
2017-05-08 14:54:47 -0500

    added setUpClass for Customer

--- Tests/test_customer.py
+++ Tests/test_customer.py
@@ -3,28 +3,21 @@ from models.Customer import *
 
 class TestCustomerMethods(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(self):
+	       self.customer = {"name": "Jessica Jr", "address": "500 Interstate Blvd S", "phone_number": "615.555.1234"}
+
     def test_create_customer(self):
 
-        customer = {
-            'name': 'Gil',
-            'address': '123 fleet st',
-            'phone' : 8085851414
-        }
-
-
-        create_customer(customer)
+        create_customer(self.customer)
         # customer_id = get_customer_id(customer['name'], customer['address'], customer['phone'])
         customer_id = get_customer_id
         self.assertIsNotNone(customer_id)
 
 
     def test_activate_customer(self):
-        customer = {
-            'name': 'Gil',
-            'address': '123 fleet st',
-            'phone' : 8085851414
-        }
-        create_customer(customer)
+
+        create_customer(self.customer)
         customer_id = get_customer_id
         active_customer = activate_customer(customer_id)
         # active_customer = 1
