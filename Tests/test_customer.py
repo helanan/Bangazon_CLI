e44fe5d6b94008c3039223088136ea7ea0f3ea15
short880 <shortjustin880@gmail.com>
2017-05-09 15:04:49 -0500

    create TestPaymentTypeMethod, change parameters to match updated format

--- Tests/test_customer.py
+++ Tests/test_customer.py
@@ -1,9 +1,9 @@
 import unittest
 from models.Customer import *
 from models.Bangazon_Operator import *
+from models.payment_types import *
 
 class TestCustomerMethods(unittest.TestCase):
-
 
 
     def test_create_customer(self):
@@ -19,3 +19,30 @@ class TestCustomerMethods(unittest.TestCase):
         active_customer = activate_customer(customer_id)
         # active_customer = 1
         self.assertIsNotNone(active_customer)
+
+class TestPaymentMethods(unittest.TestCase):
+
+	@classmethod
+	def setUpClass(self):
+		self.customer = {"name": "Jessica Jr", "address": "500 Interstate Blvd S", "city": "Nashville", "c_state": "TN", "zipcode": "37209", "phone": "615.555.1234"}
+		self.payment_type = {"account_name": "MasterCard", "account_number": "123456789098"}
+
+	def test_create_payment_type(self):
+
+		self.payment_type = {"account_name": "MasterCard", "account_number": "123456789098"}
+		create_customer(self.customer["name"], self.customer['address'], self.customer["city"], self.customer["c_state"], self.customer["zipcode"], self.customer["phone"])
+		customer_id = get_customer_id
+
+		self.assertIsNotNone(customer_id)
+
+		activate_customer(2)
+
+		active_customer = activate_customer(self)
+
+		self.assertIsNotNone(active_customer)
+
+		create_payment_type(self.payment_type['account_name'], self.payment_type['account_number'], active_customer)
+
+		payment_id = get_payment_type(self.payment_type['account_name'], self.payment_type['account_number'], active_customer)
+
+		self.assertIsNotNone(payment_id)
