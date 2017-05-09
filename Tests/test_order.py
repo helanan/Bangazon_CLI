e66bb7d2fb59b558694565ea8f7f6bf695337544
Miriam Rozenbaum <miriamrozenbaum@Miriams-MacBook-Pro.local>
2017-05-09 15:18:20 -0500

    all tests are passing in test_order.py

--- Tests/test_order.py
+++ Tests/test_order.py
@@ -10,10 +10,25 @@ class TestOrderMethods(unittest.TestCase):
 
     @classmethod
     def setUpClass(self):
-        self.order = {"customer_id" :3, "payment_id" :2, "active_status" :True, 'total': 1.00,'order_is_complete': 'Y', 'payment_type' :'Visa'}
-        self.product = {"product_title" : "shoes", "product_price" :25, "product_description":"you can put this on your feet"}
-        self.customer = {'customer_id' : 3}
-        self.payment_type = {'payment_type' :'Visa','payment_id':1 }
+        self.order = {
+        'order_id': 1,
+        "customer_id":3,
+        'payment_id':2,
+        'active_status':True,
+        'total': 1.00,
+        'payment_type' :'Visa'}
+
+        self.product = {
+        "product_title":"shoes",
+        "product_price" :25,
+        "product_description":"you can put this on your feet"}
+
+        self.customer = {
+        'customer_id' : 3}
+
+        self.payment_type = {
+        'payment_type':'Amax',
+        'payment_id':1 }
 
     def test_product_is_added_to_shopping_cart(self):
         create_order(self.order)
@@ -52,10 +67,18 @@ class TestOrderMethods(unittest.TestCase):
         customer_id = get_customer_id
         active_customer = activate_customer(customer_id)
         self.assertIsNotNone(active_customer)
-        order_complete = get_payment_type(payment_id)
-        payment_id = create_payment_type(self.payment_type)
-        payment_type = get_payment_type(payment_id)
-        self.assertIsNotNone(payment_id)
+        # create an order- adds it to our database
         create_order(self.order)
-        order_complete = assertIsNotNone(customer_id, payment_id, product_id)
-      
+        # adds payment type to our database
+        create_payment_type(self.payment_type)
+        # retrieves our orderid from order we made
+        order_id = get_order_id(self.order)
+        payment_id = get_payment_type_id(self.payment_type)
+
+        # attach a payment type to order
+        add_payment_to_order(payment_id, order_id)
+        # adds payment to or order using the order id
+        complete_order_payment_type = get_payment_type_from_order(order_id)
+       # assert the payment type is there
+        self.assertIsNotNone(complete_order_payment_type)
+
