0172aa151464db14405a82d67e4e7859239ba179
Maxwell C Baldridge <max.baldridge@gmail.com>
2017-05-10 14:34:41 -0500

    new methods for returning customer payment_types and setting a payment_type

--- models/payment_types.py
+++ models/payment_types.py
@@ -1,5 +1,12 @@
+
 def create_payment_type(account_name, account_number, active_customer):
 	pass
 
-def get_payment_type(account_name, account_number, active_customer):
-	return 2
+def get_payment_type(account_name, account_number):
+	return 1
+
+def get_customer_payment_type(customer_id):
+	return [("MasterCard", "123456789098")]
+
+def set_payment_type(payment_id):
+	pass
